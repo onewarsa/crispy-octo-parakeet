2e9c2dbbf00b2c481b24b46cd29e32d4945138ec
onewarsa <84249413+onewarsa@users.noreply.github.com>
2021-05-23 01:53:57 +0800

    Delete ergo.sh

--- ergo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-POOL=stratum+tcp://ergo-us-east1.nanopool.org:11111
-WALLET=9feHfjZXGQTuB7QFHDq19ytpzrH2DsTcha5V2T5eBmtxU8jpDJw
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
-
-cd "$(dirname "$0")"
-
-chmod +x ./arni && sudo ./arni -a ergo -o $POOL -u $WALLET.$WORKER $@
